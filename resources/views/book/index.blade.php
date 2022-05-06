d7e7c44fdce50349e25331902fb97ae9fd6673a5
Kayke Galdiano <kaykegaldiano@gmail.com>
2022-05-05 22:13:15 -0300

    adding session message and edit and delete buttons

--- resources/views/book/index.blade.php
+++ resources/views/book/index.blade.php
@@ -12,25 +12,40 @@
     <div class="container">
         <div class="row justify-content-center pt-5">
             <div class="col-md-8">
+                @if (session('message'))
+                    <div class="alert alert-success">
+                        {{ session('message') }}
+                    </div>
+                @endif
                 <div class="card">
                     <div class="card-header">List of Books</div>
                     <div class="card-body">
-                        <table class="table table-hover">
+                        <table class="table table-responsive table-hover">
                             <thead>
                               <tr>
-                                <th scope="col">#</th>
                                 <th scope="col">Name</th>
                                 <th scope="col">Description</th>
                                 <th scope="col">Category</th>
+                                <th scope="col">Edit</th>
+                                <th scope="col">Delete</th>
                               </tr>
                             </thead>
                             <tbody>
                                 @forelse ($books as $book)
                                     <tr>
-                                        <th scope="row">{{ $book->id }}</th>
                                         <td>{{ $book->name }}</td>
                                         <td>{{ $book->description }}</td>
                                         <td>{{ $book->category }}</td>
+                                        <td>
+                                            <a href="{{ route('book.edit', $book->id) }}" class="btn btn-info">Edit</a>
+                                        </td>
+                                        <td>
+                                            <form action="{{ route('book.destroy', $book->id) }}" method="POST">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-danger">Delete</button>
+                                            </form>
+                                        </td>
                                     </tr>
                                     @empty
                                     <td>No book registered!</td>
