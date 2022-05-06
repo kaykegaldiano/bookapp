19188430ad1fd1a2db5a70198cd685cb7cb8d2f5
Kayke Galdiano <kaykegaldiano@gmail.com>
2022-05-05 22:00:47 -0300

    creating update and destroy routes

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,5 @@ Route::get('/books', [BookController::class, 'index'])->name('book.index');
 Route::get('/books/create', [BookController::class, 'create'])->name('book.create');
 Route::post('/books/store', [BookController::class, 'store'])->name('book.store');
 Route::get('/books/{id}/edit', [BookController::class, 'edit'])->name('book.edit');
+Route::match(['put', 'patch'], '/books/{id}', [BookController::class, 'update'])->name('book.update');
+Route::delete('/books/{id}', [BookController::class, 'destroy'])->name('book.delete');
