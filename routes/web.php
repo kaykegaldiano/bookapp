f3a900568594e6cb930ef1b60a6f4fff922e7fc4
Kayke Galdiano <kaykegaldiano@gmail.com>
2022-05-13 17:11:38 -0300

    adding authentication routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookController;
+use App\Models\Book;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,12 +18,28 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $books = Book::paginate(5);
+
+    if (Auth::check()) {
+        return view('book.index', compact('books'));
+    }
+
+    return view('auth.login');
 });
 
-Route::get('/books', [BookController::class, 'index'])->name('book.index');
-Route::get('/books/create', [BookController::class, 'create'])->name('book.create');
+// books
+Route::get('/books', [BookController::class, 'index'])->name('book.index')->middleware('auth');
+Route::get('/books/create', [BookController::class, 'create'])->name('book.create')->middleware('auth');
 Route::post('/books/store', [BookController::class, 'store'])->name('book.store');
-Route::get('/books/{id}/edit', [BookController::class, 'edit'])->name('book.edit');
+Route::get('/books/{id}/edit', [BookController::class, 'edit'])->name('book.edit')->middleware('auth');
 Route::match(['put', 'patch'], '/books/{id}', [BookController::class, 'update'])->name('book.update');
 Route::delete('/books/{id}', [BookController::class, 'destroy'])->name('book.destroy');
+
+// login, register, authentication
+Route::get('/login', [AuthController::class, 'loginPage'])->name('auth.login');
+Route::post('/login', [AuthController::class, 'login'])->name('auth.authenticate');
+
+Route::get('/register', [AuthController::class, 'registerPage'])->name('auth.register');
+Route::post('/register', [AuthController::class, 'register'])->name('auth.store');
+
+Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
