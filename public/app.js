4aeda16e7a7fd1638c589151bedf31e7412d932e
Kayke Galdiano <kaykegaldiano@gmail.com>
2022-05-13 17:09:53 -0300

    deleting a piece of code

--- public/app.js
+++ public/app.js
@@ -3,14 +3,3 @@ function deleteMessage(book, e) {
 
     if (confirmMsg === false) e.preventDefault()
 }
-
-// dynamic active class on menu
-let navLinks = document.getElementsByClassName('nav-link')
-let current = 0
-for (var i = 0; i < navLinks.length; i++) {
-    if (navLinks[i].href === document.URL) {
-        current = i
-    }
-}
-
-navLinks[current].className += ' active'
