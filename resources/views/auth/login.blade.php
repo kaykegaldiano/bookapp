fa8b7560219bfdcd4a1231142f101d9907e3559f
Kayke Galdiano <kaykegaldiano@gmail.com>
2022-05-13 17:25:56 -0300

    adding error messages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,6 +8,11 @@
             {{ session('message') }}
         </div>
     @endif
+    @error('error')
+        <div class="alert alert-danger" role="alert">
+            {{ $message }}
+        </div>
+    @enderror
     <div class="card">
         <div class="card-header">Login</div>
         <div class="card-body">
@@ -15,11 +20,21 @@
                 @csrf
                 <div class="col-12">
                     <label for="email" class="form-label">Email</label>
-                    <input type="email" name="email" id="email" class="form-control">
+                    <input type="email" name="email" id="email" class="form-control @error('email') is-invalid @enderror">
+                    @error('email')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>                        
+                    @enderror
                 </div>
                 <div class="col-12">
                     <label for="password" class="form-label">Password</label>
-                    <input type="password" name="password" id="password" class="form-control">
+                    <input type="password" name="password" id="password" class="form-control @error('password') is-invalid @enderror">
+                    @error('password')
+                        <div class="invalid-feedback">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
                 <div class="col-12">
                     <button type="submit" class="btn btn-primary">Login</button>
